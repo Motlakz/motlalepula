360498bc3ef61b6bc179ee22d052e98c8eb5744a
Motlakz <motlalepulasello5@gmail.com>
2024-02-18 22:41:35 +0200

    add project content

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const modeButton = document.querySelector('.mode');
 const moonIcon = document.querySelector('.moon');
 const modeIcon = document.querySelector('.mode');
 
+// Check if dark mode is initially set
+const isDarkMode = body.classList.contains('dark');
+
+// Set the initial icon based on the mode
+moonIcon.classList.toggle('sun', !isDarkMode);
+
 modeButton.addEventListener('click', function () {
-    body.classList.toggle('light');
+    body.classList.toggle('dark');
     moonIcon.classList.toggle('sun');
     modeIcon.classList.toggle('day');
 });
